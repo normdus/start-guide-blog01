3a1626c8cebe790e6f8c11c2670096578383301c
Norman Duchene <norman.duchene@comcast.net>
2017-02-27 10:28:00 -0500

    5.9 Adding links done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,32 @@
 class ArticlesController < ApplicationController
 
+	# Action order index, show, new, edit, create, update, destroy.
+	# These are public methods and must be coded before private methods.
+
+	
+	# list all the articles. The route for this:
+
+	# 	articles GET /article(.:format) articles#index <----
+
+	# The route with articles#index takes us to this controller 
+	# for the index action. This is all rails convention. 
+	# follow the convention and shit works! How easy is that!
+
+	# Next we need to create a view named index.html.erb. Again this is
+	# another Rails convention.
+	def index
+		@article = Article.all
+	end
+
+	# We use Article.find to find the article we're interested in, 
+	# passing in the params[:id] to get the :id parameter from the request.
+	# We also use an instance variable (prefixed with @ to hold a 
+	# reference to the article object.)
+	def show
+		@article = Article.find(params[:id])
+	end	
+
+	# Not sure why there is nothing in the new action?
 	def new
 	end
 
@@ -10,11 +37,8 @@ class ArticlesController < ApplicationController
 		redirect_to @article		
 	end
 
-	def show
-		@article = Article.find(params[:id])
-	end
-	
-	# Strong Parameters
+	# private methods can only be used inside a class.
+	# Strong Parameters for security.
 	private
 		def article_params
 			params.require(:article).permit(:title, :text)
